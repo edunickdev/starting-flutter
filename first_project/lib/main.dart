79b43a38e1983d93ad905d2e5cd35eb62179b332
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2023-03-02 17:09:40 -0500

    first commit

---
+++ first_project/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first App"),
+        ),
+        body: Row(
+          children: [
+            Text('The Question?'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
